43ef1a0fbf1af7730b86cb433b0cc80b01566a26
alinahyder <alina@granitewood.com>
2024-06-03 23:40:43 -0400

    init commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double springCost= 309.67;
+        double summerCost = 258.35;
+        double fallCost = 178.40;
+        double winterCost = 385.90;
+        double totalCost;
+
+        totalCost= springCost+summerCost+fallCost+winterCost;
+        System.out.println("The total yearly maintenance cost is "+totalCost);
+
+
+    }
+}
